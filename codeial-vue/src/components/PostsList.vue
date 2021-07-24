d5a598985268376fb02717e5d24ce7bd8009face
yash-slashcoder <yashpatel7615@gmail.com>
2021-07-24 15:22:23 +0530

    commit vue setup

---
+++ codeial-vue/src/components/PostsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="posts-list">
+    <h1>Codeial Posts</h1>
+    <ul>
+        <li v-for="post in posts" :key='post._id' class="post-item">
+            {{post.content}} --> {{post.user.name}}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "PostsList",
+  data(){
+      return{
+          posts:[]
+      }
+  },
+  mounted(){
+      axios.get('http://localhost:8001/api/v1/posts').then((data) => {this.posts = data.data.posts})
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
